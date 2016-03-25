5ce46a502e05ea9e58b61e26752216adbcc9762b
cytangjoseph <>
2016-03-26 01:51:36 +0800

    change interface from AdderLocal to AdderRemote. Move this file to average-remote

--- average-client/src/java/comps368/u6/AveragerClient.java
+++ average-client/src/java/comps368/u6/AveragerClient.java
@@ -4,9 +4,8 @@ import javax.ejb.EJB;
 public class AveragerClient {
     @EJB
     private static AveragerRemote averager;
-    //private static AdderRemote addByRemote;
+    
     public static void main(String[] args) {
-        //System.out.println("sum is: "+ addByRemote.add(1,2,3,4));
         System.out.println(averager.average(1,2,3,4));
     }
 }
