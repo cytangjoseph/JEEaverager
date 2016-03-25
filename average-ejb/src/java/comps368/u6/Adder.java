37564bc295c185d7fb3bab3de30bd02b2d6abf9f
cytangjoseph <>
2016-03-26 01:10:59 +0800

    change interface from AdderLocal to AdderRemote. Move this file to average-remote

--- average-ejb/src/java/comps368/u6/Adder.java
+++ average-ejb/src/java/comps368/u6/Adder.java
@@ -1,7 +1,10 @@
 package comps368.u6;
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+@LocalBean
 @Stateless
+
 public class Adder implements AdderRemote {
 
     @Override
--- average-ejb/src/java/comps368/u6/Averager.java
+++ average-ejb/src/java/comps368/u6/Averager.java
@@ -5,7 +5,7 @@ import javax.ejb.Stateless;
 @Stateless
 public class Averager implements AveragerRemote {
     @EJB
-    private AdderRemote adder; //how to access?
+    private Adder adder; //how to access?
     
     @Override
     public double average(int... numbers) {
